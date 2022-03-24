9b57adff5e6dbc972095f536cc4f6850171cb241
JuanDa-dev <juanquiro23@hotmail.com>
2022-03-23 21:47:50 -0500

    Feat: Structure beggining

---
+++ Entrega1/boat.py
@@ -0,0 +1,24 @@
+#Boats in the battleship
+
+class Boat: 
+    
+    
+    #Submarine size onboard is 1 
+    #DestructorBoat size onboard is 2
+    #Cruiser size onboard is 3
+    #aircraftCarrier size onboard is 4
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def getName(self):
+        return self.name
+    
+    def getSize(self):
+        return self.size
+    
+    def boatLocation(self):
+        return self.location
+    
+    
--- Entrega1/main.py
+++ Entrega1/main.py
@@ -1,6 +1,13 @@
-a = 2 
-b = 5
+#BattleShip 
 
-c = a + b
+import pygame
 
-print   (c)
+from pygame.locals import *
+
+def constructor():
+    pygame.init()
+    pygame.display.set_caption("BattleShip")
+    screen = pygame.display.set_mode((850, 720))
+    #setting an existing img for the background
+    screen.blit(pygame.image.load("bg_1.jpg"), (0, 0))
+    return screen
