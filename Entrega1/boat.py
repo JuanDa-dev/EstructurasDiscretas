86febfa03b56be70d42d7fd5a959dca0a7605b7e
MT1120 <mateoanaya6@gmail.com>
2022-03-27 14:28:48 -0500

    Base del juego

--- Entrega1/boat.py
+++ Entrega1/boat.py
@@ -1,24 +1,31 @@
-#Boats in the battleship
+# Boats in the battleship
 
-class Boat: 
-    
-    
-    #Submarine size onboard is 1 
-    #DestructorBoat size onboard is 2
-    #Cruiser size onboard is 3
-    #aircraftCarrier size onboard is 4
-    
-    def __init__(self) -> None:
-        pass
-    
-    
+class Boat:
+
+    # Submarine size onboard is 1
+    # DestructorBoat size onboard is 2
+    # Cruiser size onboard is 3
+    # aircraftCarrier size onboard is 4
+
+    def __init__(self, x, y, size, name):
+        self.x = x
+        self.y = y
+        self.size = size
+
+        self.name = name
+
+    '''def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.size, self.size))'''
+
     def getName(self):
         return self.name
-    
+
     def getSize(self):
         return self.size
-    
-    def boatLocation(self):
-        return self.location
-    
-    
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, WIN, IMG):
+        WIN.blit(IMG, (self.x, self.y))
--- Entrega1/main.py
+++ Entrega1/main.py
@@ -1,13 +1,71 @@
-#BattleShip 
-
+# BattleShip
+import boat
 import pygame
-
+import os
+from pygame import mixer
 from pygame.locals import *
+import math
+import random
 
-def constructor():
+
+win_width = 850
+win_heigh = 720
+
+# Resource paths
+
+current_path = os.path.dirname(__file__)  # Where your .py file is located
+resource_path = os.path.join(current_path, '')  # The resource folder path
+image_path = os.path.join(resource_path, 'Assets')  # The image folder path
+
+# prepare the resources
+battleship_IMGS = pygame.transform.scale2x(
+    pygame.image.load(os.path.join(image_path, 'cruiser.png')))
+BG = pygame.transform.scale(pygame.image.load(os.path.join(
+    image_path, "bg_1.jpg")), (win_width, win_heigh))
+
+WIN = pygame.display.set_mode((win_width, win_heigh))
+
+
+# name of the game
+pygame.display.set_caption("Battle Ship")
+# icon
+icon = pygame.image.load(os.path.join(image_path, 'destructor.png'))
+pygame.display.set_icon(icon)
+
+
+# defining the ships
+# boat1 = boat.Boat(100, 100, 1, "Submarine")
+
+
+'''def constructor():
     pygame.init()
     pygame.display.set_caption("BattleShip")
     screen = pygame.display.set_mode((850, 720))
     #setting an existing img for the background
     screen.blit(pygame.image.load("bg_1.jpg"), (0, 0))
-    return screen
+    return screen'''
+
+
+def draw_win(win, boat):
+
+    win.blit(BG, (0, 0))
+
+    boat.draw(win, battleship_IMGS)
+
+    pygame.display.update()
+
+
+def game_loop():
+    boat1 = boat.Boat(100, 100, 1, "Submarine")
+
+    running = True
+    while running:
+        draw_win(WIN, boat1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+
+game_loop()
