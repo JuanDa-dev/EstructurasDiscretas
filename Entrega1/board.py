c214ebeade161aaf7c18efd0f2444936a6a16bd9
MT1120 <mateoanaya6@gmail.com>
2022-04-03 12:08:58 -0500

    Board

--- Entrega1/board.py
+++ Entrega1/board.py
@@ -1,107 +1,147 @@
 from random import randint
 from boat import Boat
+import pygame
+from pygame import mixer
+from pygame.locals import *
 
-# Genera un posicion aleatoria en el tablero
-def random_position(n):
-    return randint(0, n - 1), randint(0, n - 1)
 
-# Creo un tablero vacio
-def createBoard(n):
-    return [[0 for j in range(n)] for i in range(n)]
+WIDTH = 800
+HEIGH = 800
+FPS = 60
 
-# Revisa cuantos lugares estan vacios
-def spaces(board):
-    sum = 0
-    for t in board:
-        sum += t.count(0)
 
-    return sum
+ROWS, COL, n = 8, 8, 8
+SQUARE_SIZE = WIDTH//COL
 
-# cuanta los espacios que ocupan todos los barcos
-def num_positions(boats):
-    counter = 0
-    for b in boats:
-        counter += b.getSize()
-    return counter
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+DARK_BLUE = (0, 0, 139)
 
-# posiciona los barcos en el tablero
-def position_ships(board, boats, counter):
-    if counter == len(boats):
-        return 1
 
-    x, y = random_position(len(board))
+class Board:
+    def __init__(self):
+        #self.board = [[0 for j in range(n)] for i in range(n)]
+        self.boats = []
+        self.boat_type = {"submarines": 1, "destructorBoat": 2,
+                          "cruises": 3, "aircraftCarrier": 4}
 
-    while board[x][y] != 0:
-        x, y = random_position(len(board))
+    def draw_board(self, win):
+        win.fill(BLUE)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, DARK_BLUE, (row * SQUARE_SIZE,
+                                 col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-    if not boats[counter].locate(board, 0, x, y):
-        return 0
+    # Genera un posicion aleatoria en el tablero
 
-    if position_ships(board, boats, counter + 1) == 1:
-        return 1
+    def random_position(self, n):
+        return randint(0, n - 1), randint(0, n - 1)
 
-# Devuelve el tablero con los barcos posicionados (si es posible hacerlo)
-def battleship(boats, n):
-    for b in boats:
-      if n < b.getSize():
-        return None
+    # Creo un tablero vacio
 
-    counter = num_positions(boats)
-    if counter > n**2:
-        return None
-    else:
-        board = createBoard(n)
-        position_ships(board, boats, 0)
-        availables = spaces(board)
+    def createBoard(self, n):
+        return [[0 for j in range(n)] for i in range(n)]
 
-        while availables != n**2 - counter:
-            board = createBoard(n)
-            position_ships(board, boats, 0)
-            availables = spaces(board)
+    # Revisa cuantos lugares estan vacios
 
-        return board
+    def spaces(self, board):
+        sum = 0
+        for t in board:
+            sum += t.count(0)
 
-# Recibe una coordenada de disparo
-def shot(board, boats, x, y):
-    if board[x][y] == -1:
+        return sum
+
+    # cuanta los espacios que ocupan todos los barcos
+
+    def num_positions(self, boats):
+        counter = 0
+        for b in boats:
+            counter += b.getSize()
+        return counter
+
+    # posiciona los barcos en el tablero
+
+    def position_ships(self, board, boats, counter):
+        if counter == len(boats):
+            return 1
+
+        x, y = self.random_position(len(board))
+
+        while board[x][y] != 0:
+            x, y = self.random_position(len(board))
+
+        if not boats[counter].locate(board, 0, x, y):
+            return 0
+
+        if self.position_ships(board, boats, counter + 1) == 1:
+            return 1
+
+    # Devuelve el tablero con los barcos posicionados (si es posible hacerlo)
+
+    def battleship(self, boats, n):
+        for b in boats:
+            if n < b.getSize():
+                return None
+
+        counter = self.num_positions(boats)
+        if counter > n**2:
+            return None
+        else:
+            board = self.createBoard(n)
+            self.position_ships(board, boats, 0)
+            availables = self.spaces(board)
+
+            while availables != n**2 - counter:
+                board = self.createBoard(n)
+                self.position_ships(board, boats, 0)
+                availables = self.spaces(board)
+
+            return board
+
+    # Recibe una coordenada de disparo
+
+    def shot(self, board, boats, x, y):
+        if board[x][y] == -1:
+            return False
+
+        boat = None
+        id = board[x][y]
+
+        for b in boats:
+            if b.getId() == id:
+                boat = b
+                break
+
+        board[x][y] = -1
+
+        if boat == None:
+            return False
+
+        if not boat.isDestroyed():
+            boat.addPointDestroyed((x, y))
+            return True
+
         return False
 
-    boat = None
-    id = board[x][y]
+    # Revisa si queda algun barco sin destruir
 
-    for b in boats:
-        if b.getId() == id:
-            boat = b
-            break
-
-    board[x][y] = -1
-
-    if boat == None:
-      return False
-
-    if not boat.isDestroyed():
-        boat.addPointDestroyed((x, y))
+    def isEnd(self, boats):
+        for b in boats:
+            if not b.isDestroyed():
+                return False
         return True
 
-    return False
+    # genera el numero de barcos segun el tipo:
+    # paremetros:
+    # name = nombre del barco
+    # n = numero de barcos de ese tipo
 
-# Revisa si queda algun barco sin destruir
-def isEnd(boats):
-    for b in boats:
-        if not b.isDestroyed():
-            return False
-    return True
+    def addBoats(self, name, n):
+        self.boats = []
+        m = self.boat_type[name]
 
-boat_type = {"submarines": 1,"destructorBoat": 2, "cruises": 3, "aircraftCarrier": 4}
-# genera el numero de barcos segun el tipo:
-# paremetros:
-# name = nombre del barco
-# n = numero de barcos de ese tipo
-def addBoats(name, n):
-  boats = []
-  m = boat_type[name]
+        for i in range(n):
+            self.boats.append(Boat(name, m, 10 * m + i + 1))
 
-  for i in range(n):
-    boats.append(Boat(name, m, 10 * m + i + 1))
-  
-  return boats
+        return self.boats
--- Entrega1/main.py
+++ Entrega1/main.py
@@ -1,5 +1,4 @@
-# BattleShip
-from board import battleship, shot
+#from board import battleship, shot
 from tkinter import Button
 import boat
 import pygame
@@ -9,16 +8,7 @@ from pygame import mixer
 from pygame.locals import *
 import math
 import random
-
-
-pygame.init()
-
-mainClock = pygame.time.Clock()
-
-
-win_width = 850
-win_heigh = 720
-
+from board import Board
 
 # Resource paths
 
@@ -26,34 +16,55 @@ current_path = os.path.dirname(__file__)  # Where your .py file is located
 resource_path = os.path.join(current_path, '')  # The resource folder path
 image_path = os.path.join(resource_path, 'Assets')  # The image folder path
 
+
+pygame.init()
+
+mainClock = pygame.time.Clock()
+
+
+WIDTH = 800
+HEIGH = 800
+FPS = 60
+
+
+ROWS, COL = 8, 8
+SQUARE_SIZE = WIDTH//COL
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+DARK_BLUE = (0, 0, 139)
+
 # prepare the resources
-battleship_IMGS = pygame.transform.scale2x(
+BATTLESHIP_IMGS = pygame.transform.scale2x(
     pygame.image.load(os.path.join(image_path, 'cruiser.png')))
 
 BG = pygame.transform.scale(pygame.image.load(os.path.join(
-    image_path, "bg_3.jpg")), (win_width, win_heigh))
+    image_path, "bg_3.jpg")), (WIDTH, HEIGH))
 
 BG_MAIN_MENU = pygame.transform.scale(pygame.image.load(os.path.join(
-    image_path, "mainWindows.jpg")), (win_width, win_heigh))
+    image_path, "mainWindows.jpg")), (WIDTH, HEIGH))
 
-WIN = pygame.display.set_mode((win_width, win_heigh))
 
 BUTTON_PLAY = pygame.transform.scale(pygame.image.load(os.path.join(
     image_path, "play_BTN.png")), (390, 130))
+
+ICON = pygame.image.load(os.path.join(image_path, 'destructor.png'))
+
+WIN = pygame.display.set_mode((WIDTH, HEIGH))
 
 
 # name of the game
 pygame.display.set_caption("BattleShip")
 # icon
-icon = pygame.image.load(os.path.join(image_path, 'destructor.png'))
-pygame.display.set_icon(icon)
+pygame.display.set_icon(ICON)
 
 
 def draw_win(win, boat):
 
     win.blit(BG, (0, 0))
 
-    boat.draw(win, battleship_IMGS)
+    #boat.draw(win, battleship_IMGS)
 
     pygame.display.update()
 
@@ -69,7 +80,7 @@ def main_menu():
         print(mx, my)
 
         Button_play = WIN.blit(
-            BUTTON_PLAY, (win_width/2 - 200, 500))
+            BUTTON_PLAY, (WIDTH/2 - 200, 500))
         '''button_1 = pygame.Rect(50, 100, 200, 50)
 
         pygame.draw.rect(WIN, (255, 0, 0), button_1)'''
@@ -90,18 +101,28 @@ def game_loop():
 
     running = True
 
-    def redraw_window():
+    clock = pygame.time.Clock()
+
+    board = Board()
+
+    '''def redraw_window():
         WIN.blit(BG, (0, 0))
         #boat1.draw(WIN, battleship_IMGS)
-        pygame.display.update()
+        pygame.display.update()'''
     while running:
         #draw_win(WIN, boat1)
-        redraw_window()
+        # redraw_window()
+
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()  # quit the screen
                 running = False
+                sys.exit()
+        board.draw_board(WIN)
+        pygame.display.update()
 
-    pygame.quit()
+    # pygame.quit()
 
 
 # game_loop()
