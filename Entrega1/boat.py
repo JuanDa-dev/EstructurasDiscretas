d187cf1ea56b6f07690013bfebba08bd82a8f2f4
MT1120 <mateoanaya6@gmail.com>
2022-04-02 21:53:07 -0500

    Main_Menu

--- Entrega1/boat.py
+++ Entrega1/boat.py
@@ -7,7 +7,7 @@ class Boat:
     # Cruiser size onboard is 3
     # AircraftCarrier size onboard is 4
 
-    def __init__(self, name : str, size : int, id : int):
+    def __init__(self, name: str, size: int, id: int):
         self._name = name
         self._size = size
         self._id = id
@@ -52,7 +52,7 @@ class Boat:
     def locate(self, board, counter, x, y, location=""):
         if counter == self._size:
             return True
-        
+
         if (0 <= x < len(board)) and (0 <= y < len(board)) and board[x][y] == 0:
             board[x][y] = self._id
 
@@ -72,9 +72,5 @@ class Boat:
 
         return False
 
-    """def move(self, x, y):
-        self.x = x
-        self.y = y
-
     def draw(self, WIN, IMG):
-        WIN.blit(IMG, (self.x, self.y))"""
+        WIN.blit(IMG, (self.x, self.y))
--- Entrega1/main.py
+++ Entrega1/main.py
@@ -1,17 +1,24 @@
 # BattleShip
+from board import battleship, shot
+from tkinter import Button
 import boat
 import pygame
+import sys
 import os
 from pygame import mixer
 from pygame.locals import *
 import math
 import random
 
-from board import battleship, shot
+
+pygame.init()
+
+mainClock = pygame.time.Clock()
 
 
 win_width = 850
 win_heigh = 720
+
 
 # Resource paths
 
@@ -22,30 +29,24 @@ image_path = os.path.join(resource_path, 'Assets')  # The image folder path
 # prepare the resources
 battleship_IMGS = pygame.transform.scale2x(
     pygame.image.load(os.path.join(image_path, 'cruiser.png')))
+
 BG = pygame.transform.scale(pygame.image.load(os.path.join(
     image_path, "bg_3.jpg")), (win_width, win_heigh))
 
+BG_MAIN_MENU = pygame.transform.scale(pygame.image.load(os.path.join(
+    image_path, "mainWindows.jpg")), (win_width, win_heigh))
+
 WIN = pygame.display.set_mode((win_width, win_heigh))
+
+BUTTON_PLAY = pygame.transform.scale(pygame.image.load(os.path.join(
+    image_path, "play_BTN.png")), (390, 130))
 
 
 # name of the game
-pygame.display.set_caption("Battle Ship")
+pygame.display.set_caption("BattleShip")
 # icon
 icon = pygame.image.load(os.path.join(image_path, 'destructor.png'))
 pygame.display.set_icon(icon)
-
-
-# defining the ships
-# boat1 = boat.Boat(100, 100, 1, "Submarine")
-
-
-'''def constructor():
-    pygame.init()
-    pygame.display.set_caption("BattleShip")
-    screen = pygame.display.set_mode((850, 720))
-    #setting an existing img for the background
-    screen.blit(pygame.image.load("bg_1.jpg"), (0, 0))
-    return screen'''
 
 
 def draw_win(win, boat):
@@ -57,12 +58,45 @@ def draw_win(win, boat):
     pygame.display.update()
 
 
+def main_menu():
+
+    run = True
+    while run:
+
+        WIN.blit(BG_MAIN_MENU, (0, 0))
+
+        mx, my = pygame.mouse.get_pos()
+        print(mx, my)
+
+        Button_play = WIN.blit(
+            BUTTON_PLAY, (win_width/2 - 200, 500))
+        '''button_1 = pygame.Rect(50, 100, 200, 50)
+
+        pygame.draw.rect(WIN, (255, 0, 0), button_1)'''
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Button_play.collidepoint((mx, my)):
+
+                    game_loop()
+
+    pygame.quit()
+
+
 def game_loop():
     #boat1 = boat.Boat(100, 100, 1, "Submarine")
 
     running = True
+
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+        #boat1.draw(WIN, battleship_IMGS)
+        pygame.display.update()
     while running:
         #draw_win(WIN, boat1)
+        redraw_window()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -70,4 +104,5 @@ def game_loop():
     pygame.quit()
 
 
-game_loop()
+# game_loop()
+main_menu()
