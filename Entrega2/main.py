db8ee23822e7db4555460811e31213d7aa9b40d9
MT1120 <mateoanaya6@gmail.com>
2022-04-13 12:15:22 -0500

    CSV

--- Entrega2/main.py
+++ Entrega2/main.py
@@ -1,9 +1,12 @@
 # Here we are going to work in the web scrapper and the NPL data
 
-from unittest import result
+from dataclasses import fields
 from bs4 import BeautifulSoup
 from matplotlib.pyplot import text
+import numpy as np
 import requests
+import pandas as pd
+import csv
 import re
 
 url = "https://www.bbc.com/mundo"  # url of the page to be scraped
@@ -12,24 +15,72 @@ result = requests.get(url).text
 
 doc = BeautifulSoup(result, "html.parser")
 
+title = []
+date = []
+summary = []
+
+data = []
+fields = ["title", "date", "summary"]
+
 # print(doc)
 
 # obtain the title of each article
 
-tag = doc.find(["ul"],  class_="e57qer20 bbc-10m7ymo eom0ln50")
-# Name of the article
-tag2 = tag.find_all(["a"], class_="bbc-1fxtbkn evnt13t0")
-# time of the article
-tag3 = tag.find_all(["time"], class_="bbc-2y0240 e4zesg50")
 
-'''print(tag2[0].contents)
+tag5 = doc.find_all(["div"], class_="bbc-1sk5sm2 e19k1v2h0")
 
-a = tag2[0].contents        # Name of the article
+print(tag5[0])
+print("\n")
 
-print(a[0].text)'''
 
-'''print(tag3[0])
+for i in range(31):
+    # print(tag5[i])
+    print(i)
+    print("\n")
+    temp = tag5[i].find(["a"], class_="bbc-1fxtbkn evnt13t0")
+    '''tempa = temp.contents
+    '''
+    tempa = temp.span.string
+    # print(tempa[0].text)
+    title.append(tempa)
+    print(tempa)
+    print("\n")
+    temp2 = tag5[i].find(["time"], class_="bbc-2y0240 e4zesg50")
+    temp2a = temp2['datetime']
+    date.append(temp2a)
+    print(temp2a)
+    print("\n")
+    temp3 = tag5[i].find(["p"], class_="bbc-166eyoy e1tfxkuo1")
+    temp3a = temp3.text
+    summary.append(temp3.string)
+    print(temp3a)
+    print("\n")
 
-b = tag3[0]            # Time of the article 
+    data1 = tempa
+    data2 = temp2a
+    data3 = temp3.string
+    data.append([tempa, temp2a, temp3.string])
 
-print(b['datetime'])'''
+
+'''dict = {'title': title, 'date': date, 'summary': summary}
+df = pd.DataFrame(dict)
+
+# saving the dataframe          #pandas
+df.to_csv('GFG.csv')
+
+'''
+
+
+'''with open('GFG', 'w') as f:
+
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+                                            # csv 
+    write.writerow(fields)
+    write.writerows(data)'''
+
+
+"""np.savetxt("GFG.csv", 
+           data,
+           delimiter =", ",    #numpy 
+           fmt ='% s')"""
